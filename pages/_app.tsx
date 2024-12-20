abe59bbda44efdb9cee0470595d2bd3a32b45d76
VathanakSol <vathanaksol1605@gmail.com>
2024-12-20 22:00:44 +0700

    change font to Poppins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,26 @@ import ScrollProgressBar from "@/components/ScrollProgressBar";
 import { GlobalLoader, LoadingProvider } from "@/components/Loading";
 import BackToTopButton from "@/components/BackToTopButton";
 import MaintenanceBanner from "@/components/MaintenanceBanner";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <LoadingProvider>
-        <ScrollProgressBar />
-        <MaintenanceBanner />
-        <Component {...pageProps} />
-        <BackToTopButton />
-        <GlobalLoader />
-      </LoadingProvider>
+      <main className={poppins.className}>
+        <LoadingProvider>
+          <ScrollProgressBar />
+          <MaintenanceBanner />
+          <Component {...pageProps} />
+          <BackToTopButton />
+          <GlobalLoader />
+        </LoadingProvider>
+      </main>
     </>
   );
 }
